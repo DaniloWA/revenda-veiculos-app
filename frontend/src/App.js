a2dbaeeffa0fc143a6a4aeb1a80c23467f2e7524
Danilo Oliveira <Daniloworkdev@gmail.com>
2022-06-30 23:25:35 +0100

    feat(frontend): componente loading criado e configurado redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import { createTheme, ThemeProvider } from "@mui/material";
 import { blue } from "@mui/material/colors";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./global.css";
 import { CircularProgress } from "@mui/material";
 import React, { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 import { routes as rt } from "./routes/Routes";
-
+import { ComponentsImport } from "./view/components";
 
 const theme = createTheme({
   components: {
@@ -43,6 +44,7 @@ const App = () => {
           }
         >
           <ThemeProvider theme={theme}>
+            <ComponentsImport.Loading></ComponentsImport.Loading>
             <Routes>
               <Route path={rt.home} element={<h1>home</h1>}></Route>
             </Routes>
--- frontend/src/store/reducers/rootReducer.js
+++ frontend/src/store/reducers/rootReducer.js
@@ -1,5 +1,7 @@
 import { combineReducers } from "redux";
-
-const rootReducer = combineReducers({});
+import { loadingReducer } from "./loading.reducer";
+const rootReducer = combineReducers({
+  loadingReducer,
+});
 
 export default rootReducer;
