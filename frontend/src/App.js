f8295abeb932c74dfcfb59c1cde512ca7f356e7a
Danilo Oliveira <Daniloworkdev@gmail.com>
2022-06-30 21:14:49 +0100

    feat(frontend): criado tema Material ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { TextField, Button, createTheme, ThemeProvider } from "@mui/material";
+import { blue } from "@mui/material/colors";
+
+const theme = createTheme({
+  components: {
+    MuiTextField: {
+      defaultProps: {
+        fullWidth: true,
+        variant: "outlined",
+      },
+    },
+    MuiSelect: {
+      defaultProps: {
+        fullWidth: true,
+        variant: "outlined",
+      },
+    },
+  },
+  palette: {
+    primary: {
+      main: blue[500],
+    },
+  },
+});
+
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <h1>Aplicação react</h1>
+    <Button variant="contained" color="primary">
+      Primary
+    </Button>
+    <Button variant="contained" color="secondary">
+      Secondary
+    </Button>
+    <Button variant="contained" disabled>
+      Disabled
+    </Button>
+    <Button variant="contained" color="primary" href="#contained-buttons">
+      Link
+    </Button>
+    <TextField />
+  </ThemeProvider>
+);
 
 export default App;
