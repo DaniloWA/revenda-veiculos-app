4876f25d32d288ce43ac04cb5ec7b6c79f331c59
Danilo Oliveira <Daniloworkdev@gmail.com>
2022-07-03 12:36:17 +0100

    feat: criado componente confirm e importando

--- frontend/src/view/components/index.js
+++ frontend/src/view/components/index.js
@@ -1,9 +1,11 @@
 import Loading from "./Loading";
 import Notify from "./Notify";
 import Alert from "./Alert";
+import Confirm from "./Confirm";
 
 export const ComponentsImport = {
   Loading,
   Notify,
   Alert,
+  Confirm,
 };
