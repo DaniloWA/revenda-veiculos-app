d52e648d6d2dafa11dd901c7ca44d82e0d4a1a6a
Danilo Oliveira <Daniloworkdev@gmail.com>
2022-07-07 08:59:14 +0100

    feat: implementado register reducer

---
+++ frontend/src/store/reducers/register.reducer.js
@@ -0,0 +1,32 @@
+import { actionTypes } from "../actions/register.action";
+
+const initialState = {
+  user: {
+    name: "",
+    email: "",
+    password: "",
+  },
+  success: false,
+  error: {},
+};
+
+export const registerReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case actionTypes.CHANGE:
+      return {
+        ...state,
+        user: {
+          ...state.user,
+          ...payload,
+        },
+      };
+    case actionTypes.SUCCESS:
+      return { ...state, success: payload };
+
+    case actionTypes.ERROR:
+      return { ...state, error: payload };
+
+    default:
+      return state;
+  }
+};
