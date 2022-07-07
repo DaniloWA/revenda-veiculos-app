d67d39d3fd47062860daf0eb16ebb34036c84ef7
Danilo Oliveira <Daniloworkdev@gmail.com>
2022-07-07 08:40:04 +0100

    feat: implementado logica de cadastro de usuário e verificação

--- backend/app/Http/Controllers/Auth/AuthController.php
+++ backend/app/Http/Controllers/Auth/AuthController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    public function sotre(Request $request){
+    public function store(Request $request){
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
@@ -16,7 +19,28 @@ class AuthController extends Controller
         ]);
 
         if( $validator->fails() ){
-            return response()->json(['errors'=> $validator->errors()],200);
+            return response()->json(['errors'=> $validator->errors()],400);
         }
+
+        $date = Carbon::now();
+        $deleted_accont = Carbon::now();
+
+        $user = new User;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->next_expiration = $date->addDays(7);
+        $user->deleted_accont = $deleted_accont->addDays(15);
+        $user->save();
+
+        if($user->id){
+            return response()->json([
+                'access_token' => $user->createToken('auth-api')->accessToken
+            ], 200);
+        }
+
+        return response()->json([
+            'error' => 'Erro ao cadastrar usuário'
+        ], 400);
     }
 }
