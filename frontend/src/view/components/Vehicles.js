5d263e00f8ebd61e1f011196db03152f996989fa
Danilo Oliveira <Daniloworkdev@gmail.com>
2022-07-09 21:59:45 +0100

    feat: importando header dentro de vehicles

--- frontend/src/view/components/Vehicles.js
+++ frontend/src/view/components/Vehicles.js
@@ -1,9 +1,10 @@
-import React from 'react'
-import
-
+import React from "react";
+import Header from "../components/Header";
 
 export default function Vehicles() {
   return (
-    <div>Vehicles</div>
-  )
+    <>
+      <Header />
+    </>
+  );
 }
